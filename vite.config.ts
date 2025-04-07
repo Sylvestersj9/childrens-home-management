a4e6d30b6e55b39df502b91ed228ce8c9ef62771
Sylvestersj9 <janvesylvester@gmail.com>
2025-04-07 10:55:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,8 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  build:
+  build: {
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    emptyOutDir: true,
+  },
+});
