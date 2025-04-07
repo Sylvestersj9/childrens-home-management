1b3d0d0c9767ee3c2932b88f5e498da2323f4e83
Sylvestersj9 <janvesylvester@gmail.com>
2025-04-07 10:49:11 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
@@ -26,8 +25,4 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-});
+  build:
